2a90a975b247af5a4eb7a5f9c052fcffe691a801
Brian Hackett <brian.a.hackett@gmail.com>
2021-08-16 21:40:45 +0100

    updated gift list

--- giftlist.py
+++ giftlist.py
@@ -20,12 +20,21 @@ print('''
 		If you do wish to buy us a gift, please choose from one of the options below, or donate to our chosen charities.<br>If you do pick a gift from the list below, please click the button to 'claim' it so that it is removed from the list for other guests.<br><br>Please do not feel obliged to buy us a gift or donate. Thank you very much.
 		<br><br>
 		<h2>Charities</h2>
+		<b>Crohn's &amp; Colitis UK</b>
+		<br><img src='candcuk.jpg' width='250'/>
+		<br>"We’re here to give people affected by Crohn's and Colitis hope, comfort and confidence. To make sure their voices are heard and help them live freer, fuller lives."
+		<br>Find out more <a href='https://www.crohnsandcolitis.org.uk/about-us/what-we-do' target='_blank'>here</a>.
+		<br><br>
+		<a href='https://e.crohnsandcolitis.org.uk/donations/main-donation/donate'>Donate</a>
+		<br><br>
 		<b>Samaritans</b>
 		<br><img src='./samaritans.jpg' width='250'/>
 		<br>Every day, Samaritans volunteers respond to around 10,000 calls for help.
-                <br>Find out more <a href='https://www.samaritans.org/scotland/about-samaritans/our-organisation/what-we-do/' target='_blank'>here</a>.
+		<br>Find out more <a href='https://www.samaritans.org/scotland/about-samaritans/our-organisation/what-we-do/' target='_blank'>here</a>.
 		<br><br>
 		<a href='https://www.samaritans.org/scotland/donate-now/'>Donate</a>
+		<br><br>
+		Gift list items will be added soon.
 	</body>
 </html>
 ''')
